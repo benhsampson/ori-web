3937b0d3ab7e4de32ce4fd85c5848c8e444c0378
benhsampson <benhsampson@protonmail.com>
2023-01-16 14:24:42 +1100

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { DM_Sans } from "@next/font/google";
+
+import "../styles/globals.css";
+
+// const dmSans = DM_Sans({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
 }
